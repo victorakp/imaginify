af9a6b83a137683034d9251db7dfd022b7a9bc1e
victorakp <victorakp20@gmail.com>
2024-05-18 12:01:42 +0100

    Yours is

--- Downloads/imaginify-main/components/shared/Collection.tsx
+++ Downloads/imaginify-main/components/shared/Collection.tsx
@@ -54,11 +54,12 @@ export const Collection = ({
       </div>
 
       {images.length > 0 ? (
-        <ul className="collection-list">
-          {images.map((image) => (
-            <Card image={image} key={image._id} />
-          ))}
-        </ul>
+       <ul className="collection-list">
+       {images.map((image) => (
+         // @ts-ignore
+         <Card image={image} key={image._id} />
+       ))}
+     </ul>
       ) : (
         <div className="collection-empty">
           <p className="p-20-semibold">Empty List</p>
